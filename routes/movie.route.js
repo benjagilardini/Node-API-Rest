ee56b6ff1c7eabb095a6db7b8725b5db80559f0b
Sencha <benja.gilardini@gmail.com>
2020-05-23 19:53:31 -0300

    Clase node API REST

---
+++ routes/movie.route.js
@@ -0,0 +1,43 @@
+const { Router } = require('express');
+const router = Router();
+const _ = require('lodash');
+
+const movies = require('../sample.json');
+
+router.get('/movies', (req, res) => {
+    res.json(movies);
+});
+
+router.post('/movies', (req, res) => {
+    const { title, director, year, rating } = req.body;
+    if (title && director && year && rating) {
+        const newMovie = { ...req.body };
+        movies.push(newMovie);
+        res.json({ 'added': 'ok' });
+    } else {
+        res.status(400).json({ 'statusCode': 'Bad Request' })
+    }
+});
+
+router.delete('/movies/:id', (req, res) => {
+    const id = req.params.id;
+    _.remove(movies, (movie) => {
+        return movie.id == id
+    });
+    res.json(movies);
+});
+
+router.put('/movies/:id', (req, res) => {
+    const id = req.params.id;
+    const { title, director, year, rating } = req.body;
+    _.each(movies, (movie) => {
+        if (movie.id == id) {
+            movie.title = title;
+            movie.director = director;
+            movie.year = year;
+            movie.rating = rating;
+        }
+    });
+    res.json({'modified': 'ok'});
+});
+module.exports = router;
